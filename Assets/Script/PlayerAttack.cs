6fcae93b69f6c53d8c057474608117b2a79ec597
ไม่บอกหรอก <ping31345@gmail.com>
2020-02-04 09:48:58 +0700

    try to fix attack

--- Assets/Script/PlayerAttack.cs
+++ Assets/Script/PlayerAttack.cs
@@ -5,34 +5,19 @@ using UnityEngine;
 public class PlayerAttack : MonoBehaviour
 {
     private bool attacking;
-   [SerializeField] Collider2D attackTrigger; ///use trigger/// 
+    [SerializeField] float attackRange;
+    [SerializeField] LayerMask whatIsEnemy;
+    [SerializeField] Transform attackPos;
     float attackTimer = 0;
     float attackCd = 0.3f;
+
     private void Awake()
     {
         attacking = false;
-        attackTrigger.enabled = false; //disable attack collider 
     }
 
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Z)&& !attacking)
-        {
-            attacking = true;
-            attackTimer = attackCd;
-            attackTrigger.enabled = true;
-        }
-        if(attacking)
-        {
-            if (attackTimer > 0)
-            {
-                attackTimer -= Time.deltaTime;
-            }
-            else
-            {
-                attacking = false;
-                attackTrigger.enabled = false;
-            }
-        }
+
     }
 }
