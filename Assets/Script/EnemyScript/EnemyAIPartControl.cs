f1464cee2c0db965eb56b7217d23f48e04ff3122
ไม่บอกหรอก <ping31345@gmail.com>
2020-02-11 09:14:36 +0700

    3 type of AI Fininsh

---
+++ Assets/Script/EnemyScript/EnemyAIPartControl.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyAIPartControl : MonoBehaviour
+{
+
+    AIPath aiPath;
+    [SerializeField] float playerCheckRadius;
+    [SerializeField] LayerMask whatIsPlayer;
+    float aiSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        aiPath = GetComponent<AIPath>();
+        aiSpeed = GetComponent<AIPath>().maxSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void FixedUpdate()
+    {
+        if (!playerIsInRadius())
+        {
+            aiPath.maxSpeed = Mathf.Clamp(aiPath.maxSpeed - (Time.deltaTime*3), 0, aiPath.maxSpeed);
+        }
+        else aiPath.maxSpeed = 3;
+    }
+
+    bool  playerIsInRadius()
+    {
+        return Physics2D.OverlapCircle(transform.position, playerCheckRadius, whatIsPlayer);
+    }
+
+    void OnDrawGizmosSelected() //draw circle base on this object transform.position and collider radius
+    {
+        Gizmos.color = Color.red; //Select wire color
+        Gizmos.DrawWireSphere(transform.position, playerCheckRadius);
+    }
+}
---
+++ Assets/Script/EnemyScript/EnemyMove3.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyMove3 : MonoBehaviour
+{
+    Path path;
+    Seeker seeker;
+    Rigidbody2D enemyRigid;
+    [SerializeField] Transform target;
+    int currentWayPoint = 0;
+    bool reachedEndOfPoint = false;
+    [SerializeField] float speed = 150f;
+    public float nextWayPointDistance = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        seeker = GetComponent<Seeker>();
+        enemyRigid = GetComponent<Rigidbody2D>();
+        path = GetComponent<Path>();
+        seeker.StartPath(enemyRigid.position, target.position, OnPathComplete);
+    }
+    void OnPathComplete(Path p)
+    {
+        if(!p.error)
+        {
+            path = p;
+            currentWayPoint = 0;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
