fb152a465a0bee236b97e5401cd775364557ae57
ไม่บอกหรอก <ping31345@gmail.com>
2020-04-21 06:33:18 +0700

    final before add sound

---
+++ Assets/Script/Boss3Attack.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss3Attack : MonoBehaviour
+{
+    [SerializeField] Transform attackPosition;
+    [SerializeField] GameObject attack;
+    float randomNum;
+    float spawnTime, spawnTimer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnTime = 5;
+        spawnTimer = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        randomNum = Random.Range(6f,6f);
+        spawnTimer -= Time.deltaTime;
+        Vector3 Position = new Vector3(attackPosition.transform.position.x + randomNum, attackPosition.transform.position.y + randomNum);
+        if (spawnTimer <=0)
+        {
+            Instantiate(attack, Position , Quaternion.identity);
+            spawnTimer = spawnTime;
+        }
+    }
+}
